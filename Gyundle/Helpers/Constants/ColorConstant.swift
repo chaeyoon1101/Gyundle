242ba1621f67dc7cd034a01ae592c6bc4423df12
chaeyoon1101 <pgbenny0035@gmail.com>
2024-12-23 23:15:52 +0900

    add: ColorConstant 추가

---
+++ Gyundle/Helpers/Constants/ColorConstant.swift
@@ -0,0 +1,20 @@
+//
+//  ColorConstant.swift
+//  Gyundle
+//
+//  Created by 임채윤 on 12/23/24.
+//
+
+import SwiftUI
+
+enum ColorConstant {
+    static let bgPrimary = Color.background1
+    static let bgSecondary = Color.background2
+    static let bgContent = Color.background3
+    
+    static let fgPrimary = Color.text1
+    static let fgSecondary = Color.text2
+    static let fgTertiary = Color.text3
+    
+    static let kakao = Color.kakao
+}
