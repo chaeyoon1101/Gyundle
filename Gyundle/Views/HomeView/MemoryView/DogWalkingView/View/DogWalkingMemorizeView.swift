ff704bd5b96a4692fe31c8e3473b018549ca48ce
chaeyoon1101 <pgbenny0035@gmail.com>
2025-01-14 23:19:52 +0900

    feat: 산책 데이터 보여주는 뷰 UI 구성

--- Gyundle/Views/HomeView/MemoryView/DogWalkingView/View/DogWalkingMemorizeView.swift
+++ Gyundle/Views/HomeView/MemoryView/DogWalkingView/View/DogWalkingMemorizeView.swift
@@ -9,14 +9,140 @@ struct DogWalkingMemorizeView: View {
     @State private var isMapExpanded: Bool = false
     
     var body: some View {
-        VStack {
-            DogWalkingMapView(isMapExpanded: $isMapExpanded)
-                .frame(maxHeight: isMapExpanded ? .infinity : 300)
-                .ignoresSafeArea()
+        ZStack {
+            Self.background(color: ColorConstant.bgPrimary)
             
-            Spacer(minLength: 0)
+            VStack {
+                DogWalkingMapView(isMapExpanded: $isMapExpanded)
+                    .ignoresSafeArea()
+                    .frame(maxHeight: .infinity)
+                
+                if !isMapExpanded {
+                    VStack(spacing: 15) {
+                        DogWalkingDataView()
+                        
+                        DogWalkingStopButton()
+                        
+                        DogWalkingFuctionButtons()
+                            .padding(.top, 30)
+                    }
+                    .frame(maxHeight: .infinity, alignment: .top)
+                    .background(
+                        RoundedRectangle(cornerRadius: 12)
+                            .fill(ColorConstant.bgSecondary)
+                            .ignoresSafeArea()
+                    )
+                    .padding(.horizontal, 4)
+                }
+            }
         }
-        
+    }
+    
+    @ViewBuilder
+    private func DogWalkingDataView() -> some View {
+        VStack {
+            VStack {
+                Text("34:01")
+                    .font(.system(size: 56))
+                    .bold()
+                
+                Text("산책 시간")
+                    .font(.subheadline)
+                    .foregroundStyle(ColorConstant.fgSecondary)
+            }
+            .padding(.bottom, 30)
+            
+            HStack {
+                DogWalkingDataContent(data: "1.12km", subtitle: "산책 거리")
+                
+                DogWalkingDataContent(data: "3.8km/h", subtitle: "산책 속도")
+                
+                DogWalkingDataContent(data: "7.5kcal", subtitle: "칼로릴 소모량")
+            }
+        }
+        .frame(maxWidth: .infinity)
+        .padding()
+    }
+    
+    @ViewBuilder
+    private func DogWalkingDataContent(data: String, subtitle: String) -> some View {
+        VStack {
+            Text(data)
+                .font(.title2)
+                .bold()
+            
+            Text(subtitle)
+                .font(.subheadline)
+                .foregroundStyle(ColorConstant.fgSecondary)
+        }
+        .frame(maxWidth: .infinity)
+    }
+    
+    @ViewBuilder
+    private func DogWalkingStopButton() -> some View {
+        Button {
+            
+        } label: {
+            Image(systemName: "stop.fill")
+                .resizable()
+                .aspectRatio(contentMode: .fit)
+                .foregroundStyle(ColorConstant.bgPrimary)
+                .frame(width: 32, height: 32)
+                .frame(width: 96, height: 96)
+                .background(
+                    Circle().fill(ColorConstant.fgPrimary)
+                )
+        }
+    }
+    
+    @ViewBuilder
+    private func DogWalkingFuctionButtons() -> some View {
+        HStack(spacing: 45) {
+            Button {
+                
+            } label: {
+                Image("DogPee")
+                    .resizable()
+                    .aspectRatio(contentMode: .fit)
+                    .foregroundStyle(.yellow)
+            }
+            .buttonStyle(DogWalkingFunctionsButtonStyle())
+            
+            Button {
+                
+            } label: {
+                
+                Image("DogPoop")
+                    .resizable()
+                    .aspectRatio(contentMode: .fit)
+                    .foregroundStyle(.brown)
+            }
+            .buttonStyle(DogWalkingFunctionsButtonStyle())
+            
+            Button {
+                
+            } label: {
+                Image(systemName: "camera.fill")
+                    .resizable()
+                    .aspectRatio(contentMode: .fit)
+                    .foregroundStyle(ColorConstant.fgPrimary)
+            }
+            .buttonStyle(DogWalkingFunctionsButtonStyle())
+        }
+    }
+}
+
+fileprivate struct DogWalkingFunctionsButtonStyle: ButtonStyle {
+    func makeBody(configuration: Configuration) -> some View {
+        configuration.label
+            .frame(width: 20, height: 20)
+            .frame(width: 60, height: 60)
+            .background(
+                Circle()
+                    .fill(ColorConstant.bgContent)
+                    .strokeBorder(ColorConstant.fgPrimary, lineWidth: 2)
+            )
+            .opacity(configuration.isPressed ? 0.5 : 1)
     }
 }
 
@@ -24,3 +150,4 @@ struct DogWalkingMemorizeView: View {
     DogWalkingMemorizeView()
         .environmentObject(DogWalkingViewModel())
 }
+
